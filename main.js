7dc92d98fc700dff720d4758b37c33f8400d5d03
TA2k <tombox2020@gmail.com>
2023-01-31 01:09:09 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,320 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v2.3.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios").default;
+const Json2iob = require("json2iob");
+
+class Omada extends utils.Adapter {
+  /**
+   * @param {Partial<utils.AdapterOptions>} [options={}]
+   */
+  constructor(options) {
+    super({
+      ...options,
+      name: "omada",
+    });
+    this.on("ready", this.onReady.bind(this));
+    this.on("stateChange", this.onStateChange.bind(this));
+    this.on("unload", this.onUnload.bind(this));
+    this.deviceArray = [];
+    this.updateInterval = null;
+    this.reLoginTimeout = null;
+    this.refreshTokenTimeout = null;
+    this.session = {};
+    this.json2iob = new Json2iob(this);
+    this.requestClient = axios.create();
+    this.omadacId = "";
+  }
+
+  /**
+   * Is called when databases are connected and adapter received configuration.
+   */
+  async onReady() {
+    // Reset the connection indicator during startup
+    this.setState("info.connection", false, true);
+    // if (this.config.interval < 0.5) {
+    //   this.log.info("Set interval to minimum 0.5");
+    //   this.config.interval = 0.5;
+    // }
+    if (!this.config.ip || !this.config.username || !this.config.password) {
+      this.log.error("Please set username and password in the instance settings");
+      return;
+    }
+
+    this.subscribeStates("*");
+
+    this.log.info("Login to Omada " + this.config.ip + ":" + this.config.port);
+    await this.login();
+    if (this.session.token) {
+      await this.getDeviceList();
+      await this.updateDevices();
+      this.updateInterval = setInterval(async () => {
+        await this.updateDevices();
+      }, 5 * 60 * 1000);
+    }
+    this.refreshTokenInterval = setInterval(() => {
+      this.refreshToken();
+    }, 24 * 60 * 60 * 1000);
+  }
+  async login() {
+    await this.requestClient({
+      method: "get",
+      url: `https://${this.config.ip}:${this.config.port}`,
+    })
+      .then((res) => {
+        //this.log.debug(JSON.stringify(res.data));
+        this.omadacId = res.headers.location.split("/")[1];
+        this.log.info(`Omada cID: ${this.omadacId}`);
+      })
+      .catch((error) => {
+        this.log.error(error);
+        this.log.error("Login failed");
+        error.response && this.log.error(JSON.stringify(error.response.data));
+      });
+    await this.requestClient({
+      method: "post",
+      url: `https://${this.config.ip}:${this.config.port}/${this.omadacId}/api/v2/login`,
+      headers: {
+        Accept: "application/json, text/javascript, */*; q=0.01",
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      data: {
+        username: this.config.username,
+        password: this.config.password,
+      },
+    })
+      .then((res) => {
+        //  this.log.debug(JSON.stringify(res.data));
+        if (res.data.token) {
+          this.log.info("Login successful");
+          this.session = res.data;
+          this.setState("info.connection", true, true);
+        } else {
+          this.log.error("Login failed: " + JSON.stringify(res.data));
+          return;
+        }
+      })
+      .catch((error) => {
+        this.log.error(error);
+        this.log.error("Login failed");
+        error.response && this.log.error(JSON.stringify(error.response.data));
+      });
+  }
+
+  async getDeviceList() {
+    await this.requestClient({
+      method: "get",
+      url: `https://${this.config.ip}:${this.config.port}/${this.omadacId}/api/v2/sites?currentPageSize=100&currentPage=1`,
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Csrf-Token": this.session.token,
+      },
+    })
+      .then(async (res) => {
+        this.log.debug(JSON.stringify(res.data));
+        if (res.data.result && res.data.result.data) {
+          this.log.info(`Found ${res.data.result.data.length} sites`);
+          for (const device of res.data.result.data) {
+            this.log.debug(JSON.stringify(device));
+            const id = device.id;
+
+            this.deviceArray.push(device);
+            const name = device.name;
+
+            await this.setObjectNotExistsAsync(id, {
+              type: "device",
+              common: {
+                name: name,
+              },
+              native: {},
+            });
+            await this.setObjectNotExistsAsync(id + ".remote", {
+              type: "channel",
+              common: {
+                name: "Remote Controls",
+              },
+              native: {},
+            });
+
+            const remoteArray = [{ command: "Refresh", name: "True = Refresh" }];
+            remoteArray.forEach((remote) => {
+              this.setObjectNotExists(id + ".remote." + remote.command, {
+                type: "state",
+                common: {
+                  name: remote.name || "",
+                  type: remote.type || "boolean",
+                  role: remote.role || "button",
+                  def: remote.def != null ? remote.def : false,
+                  write: true,
+                  read: true,
+                },
+                native: {},
+              });
+            });
+            this.json2iob.parse(id + ".general", device, { forceIndex: true });
+          }
+        }
+      })
+      .catch((error) => {
+        this.log.error(error);
+        error.response && this.log.error(JSON.stringify(error.response.data));
+      });
+  }
+
+  async updateDevices() {
+    const statusArray = [
+      {
+        url: "https://smartapi.vesync.com/cloud/v2/deviceManaged/bypassV2",
+        path: "status",
+        desc: "Status of the device",
+      },
+    ];
+
+    for (const element of statusArray) {
+      for (const device of this.deviceArray) {
+        // const url = element.url.replace("$id", id);
+
+        await this.requestClient({
+          method: "get",
+          url: `https://${this.config.ip}:${this.config.port}/${this.omadacId}/api/v2/${element.url}`,
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Csrf-Token": this.session.token,
+          },
+        })
+          .then(async (res) => {
+            this.log.debug(JSON.stringify(res.data));
+            if (!res.data.result) {
+              return;
+            }
+            if (res.data.errorCode != 0) {
+              this.log.error(JSON.stringify(res.data));
+              return;
+            }
+            let data = res.data.result;
+            if (data.result) {
+              data = data.result;
+            }
+
+            const forceIndex = true;
+            const preferedArrayName = null;
+
+            this.json2iob.parse(device.id + "." + element.path, data, {
+              forceIndex: forceIndex,
+              preferedArrayName: preferedArrayName,
+              channelName: element.desc,
+            });
+            await this.setObjectNotExistsAsync(element.path + ".json", {
+              type: "state",
+              common: {
+                name: "Raw JSON",
+                write: false,
+                read: true,
+                type: "string",
+                role: "json",
+              },
+              native: {},
+            });
+            this.setState(element.path + ".json", JSON.stringify(data), true);
+          })
+          .catch((error) => {
+            if (error.response) {
+              if (error.response.status === 401) {
+                error.response && this.log.debug(JSON.stringify(error.response.data));
+                this.log.info(element.path + " receive 401 error. Refresh Token in 60 seconds");
+                this.refreshTokenTimeout && clearTimeout(this.refreshTokenTimeout);
+                this.refreshTokenTimeout = setTimeout(() => {
+                  this.refreshToken();
+                }, 1000 * 60);
+
+                return;
+              }
+            }
+            this.log.error(element.url);
+            this.log.error(error);
+            error.response && this.log.error(JSON.stringify(error.response.data));
+          });
+      }
+    }
+  }
+  async refreshToken() {
+    this.log.debug("Refresh token");
+    await this.login();
+  }
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  /**
+   * Is called when adapter shuts down - callback has to be called under any circumstances!
+   * @param {() => void} callback
+   */
+  onUnload(callback) {
+    try {
+      this.setState("info.connection", false, true);
+      this.reLoginTimeout && clearTimeout(this.reLoginTimeout);
+      this.refreshTokenTimeout && clearTimeout(this.refreshTokenTimeout);
+      this.updateInterval && clearInterval(this.updateInterval);
+      this.refreshTokenInterval && clearInterval(this.refreshTokenInterval);
+      callback();
+    } catch (e) {
+      callback();
+    }
+  }
+
+  /**
+   * Is called if a subscribed state changes
+   * @param {string} id
+   * @param {ioBroker.State | null | undefined} state
+   */
+  async onStateChange(id, state) {
+    if (state) {
+      if (!state.ack) {
+        const deviceId = id.split(".")[2];
+        const folder = id.split(".")[3];
+        const command = id.split(".")[4];
+
+        this.refreshTimeout = setTimeout(() => {
+          this.updateDevices();
+        }, 5000);
+      } else {
+        // const idArray = id.split(".");
+        // const command = id.split(".")[3];
+        // const stateName = idArray[idArray.length - 1];
+        // const deviceId = id.split(".")[2];
+        // if (command === "remote") {
+        //   return;
+        // }
+        // const resultDict = {
+        //   onOff: "turn",
+        //   turn: "turn",
+        //   brightness: "brightness",
+        //   r: "r",
+        //   g: "g",
+        //   b: "b",
+        //   colorTemInKelvin: "colorwc",
+        // };
+        // if (resultDict[stateName]) {
+        //   const value = state.val;
+        //   await this.setStateAsync(deviceId + ".remote." + resultDict[stateName], value, true);
+        // }
+      }
+    }
+  }
+}
+
+if (require.main !== module) {
+  // Export the constructor in compact mode
+  /**
+   * @param {Partial<utils.AdapterOptions>} [options={}]
+   */
+  module.exports = (options) => new Omada(options);
+} else {
+  // otherwise start the instance directly
+  new Omada();
+}
